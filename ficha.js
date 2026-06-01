320eec891197fb41264b56d7f79c233d1506d529
rzbro <ry0102barbosa@gmail.com>
2026-06-01 11:54:13 +0000

    versao1

---
+++ ficha.js
@@ -0,0 +1,8 @@
+// Criar um objeto que represente o mundo real 
+const animal = {
+    nome: "Thor",      // string
+    especie: "Cachorro",// string
+    idade: 4,          // number (Vírgula adicionada aqui!)
+    tutor: true,       // boolean
+    vacinado: false    // boolean
+};
