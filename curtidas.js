4ed2e98be0c1997be100d714e5e6e948db32e820
rzbro <ry0102barbosa@gmail.com>
2026-07-06 11:52:03 +0000

    venv 1

--- curtidas.js
+++ curtidas.js
@@ -2,6 +2,35 @@ const feedJson = require ('./feed.json');
 
  function curtir(feed,id) {
     const post= feed[0].id === id ? feed [0] :
-    feed [1]. id === id ? feed [1] : feed [2]; 
- }
+    feed[1].id === id ? feed [1] : feed[2]; 
  
+    if(!post) {
+       console.log("Post não encontrado.");
+       return;
+    }
+
+    if (!post.publicado) {
+       console.log("Não é possível curtir este post");
+       return;
+    }
+
+    post.metricas.curtidas++;
+    console.log("R");
+    console.log(`Post de @${post.autor} agora tem ${post.metricas.curtidas} curtidas.`);
+ }
+
+function descurtir (feed, id) {
+    const post = feed[0].id === id ? feed [0] : feed[1].id === id ? feed[1] : feed;
+
+    if(!post) {
+       console.log("Post não encontrado.");
+       return;
+    }
+
+    if (post.metricas.curtidas === 0) {
+       console.log("Não é possível descurtir um post com zero curtidas.");
+       return;
+    }
+
+    post.metricas.curtidas--;
+}
