879f95d943d23a6bb232be2d1757eb210ad3352a
rzbro <ry0102barbosa@gmail.com>
2026-08-04 11:51:33 +0000

    venv1

---
+++ conta3.js
@@ -0,0 +1,14 @@
+class ContaBancaria{
+    #saldo;
+    constructor(titular,saldoInicial){
+        this.titular = titular;
+        this.#saldo = saldoInicial;
+    
+    }
+    depositar(valor){
+        if(valor > 0){
+            console.log("valor de deposito invalido.");]
+            return;
+        }
+    }
+}
