fda80f057a061f9af9903301eebbc2c6abc8a6c3
rzbro <ry0102barbosa@gmail.com>
2026-08-05 11:42:55 +0000

    venv1

--- conta3.js
+++ conta3.js
@@ -6,9 +6,20 @@ class ContaBancaria{
     
     }
     depositar(valor){
-        if(valor > 0){
-            console.log("valor de deposito invalido.");]
+        if(valor <= 0){
+            console.log("valor de deposito invalido.");
             return;
         }
+        this.#saldo += valor;
+    }
+    sacar (valor) {
+        if (valor > this.#saldo) {
+            console.log("saldo insuficiente.")
+            return; //pausa o processo
+        }
+        this.#saldo -= valor;
+    }
+    get saldoAtual(){
+        return this.#saldo;
     }
 }
